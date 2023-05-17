aef9377e9f6a0f8917bdefb8f8d98b684a9a8371
ponkan31 <me_me_kero127@outlook.jp>
2023-05-17 13:37:28 +0900

    タスクの追加機能（テキスト）が完成しました

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-import "style.css";
+
+const onClickAdd = () => {
+    // textの中身を取得
+    const inputText = document.getElementById("add-text").value;
+    // 入力したものが残り続けると邪魔なので空文字にする
+    document.getElementById("add-text").value = ""
+
+
+    // divを生成
+    const li = document.createElement("li");
+    // クラス名を付与
+    li.className = "list-row" ;
+
+    // ｐタグ生成
+    const p = document.createElement("p");
+    // pタグの中身は取得したinputText
+    p.innerText = inputText;
+    
+    // divタグの子要素にｐタグ生成
+    li.appendChild(p);
+    // 未完了リストに追加
+    document.getElementById("incomplete-list").appendChild(li);
+  };
+  
+  document
+    .getElementById("add-button")
+    .addEventListener("click", () => onClickAdd());
+  
